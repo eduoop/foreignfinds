f13d9df4e06792f400d36f1f229ae2af1b83308f
Eduardo <eduardo.moraes@clicksoft.com.br>
2023-08-08 23:41:54 -0300

    feat: remove random image name

--- app/UseCases/Upload/avatar.ts
+++ app/UseCases/Upload/avatar.ts
@@ -2,13 +2,11 @@
 /* eslint-disable prettier/prettier */
 import type { MultipartFileContract } from "@ioc:Adonis/Core/BodyParser";
 import { UploadImage } from "App/Services/UploadImage";
-import { randomUUID } from "crypto";
 
 export class AvatarUseCase {
   constructor(private uploadImage: UploadImage) {}
 
   async execute(image: MultipartFileContract, userId: string): Promise<string> {
-    const filename = randomUUID();
     return await this.uploadImage.uploadProfileImage(image, userId);
   }
 }

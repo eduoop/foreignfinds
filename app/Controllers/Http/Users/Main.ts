d329e347be5c66960a37af198453c7bd38a39495
Eduardo <eduardo.moraes@clicksoft.com.br>
2023-08-21 09:23:03 -0300

    fix: remove unused contexts contract

--- app/Controllers/Http/Users/Main.ts
+++ app/Controllers/Http/Users/Main.ts
@@ -8,7 +8,7 @@ export default class UsersController {
 
   public async show({ }: HttpContextContract) { }
 
-  public async update({ request, auth, params, response }: HttpContextContract) {
+  public async update({ request, params }: HttpContextContract) {
     const { name, password, phone, surname } = await request.validate(UpdateValidator)
     const user = await User.findByOrFail("id", params.id)
     await user.merge({ name, password, phone, surname })

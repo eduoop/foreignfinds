df575ea3f7616806bc86a9c9af794fbf35528b9e
Eduardo <eduardo.moraes@clicksoft.com.br>
2023-09-13 21:43:51 -0300

    fix: change origin

--- config/cors.ts
+++ config/cors.ts
@@ -44,7 +44,7 @@ const corsConfig: CorsConfig = {
   |                     one of the above values.
   |
   */
-  origin: 'https://foreingfinds-frontend.vercel.app/',
+  origin: '*',
 
   /*
   |--------------------------------------------------------------------------
